3efd661db92e0d6fc4719b4e95c7a7f9c762076b
dont-wait <nguyentansang15042005@gmail.com>
2025-05-03 12:10:28 +0700

    add source for lesson about rule for plural name, dont use verd, dont nest resource, error code with body

--- src/main/java/com/dontwait/spring_boot_non_jwt/api/BuildingAPI.java
+++ src/main/java/com/dontwait/spring_boot_non_jwt/api/BuildingAPI.java
@@ -1,20 +1,20 @@
 package com.dontwait.spring_boot_non_jwt.api;
 
-import java.lang.module.ModuleDescriptor.Requires;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dontwait.spring_boot_non_jwt.dto.Buildingdto;
+import com.dontwait.spring_boot_non_jwt.dto.ErrorResponseDto;
+import com.dontwait.spring_boot_non_jwt.exceptions.FieldRequiredExeption;
+
 
 @RestController
 public class BuildingAPI {
@@ -23,26 +23,47 @@ public class BuildingAPI {
 //		return "Hello world";
 //	}
 	
-	@GetMapping(value="api/building")
-	public Buildingdto getBuilding(@RequestParam(value="name", required = false) String name,
-							@RequestParam(value="numberOfBasement", required = false) Integer noB,
-							@RequestParam(value="ward", required = false) String ward,
-							@RequestParam(value="street", required = false) String street) {
+	
+	//Neu tren client gui ve CO' KEY vidu "name=" nhung KO CO VALUE :)) 
+	//Nhan vao empty
+	
+	//vay neu khong co key lan value thi... NULL
+	
+	//TRONG JAVA CO 2 TH KHI NHAN DU LIEU TU PARAM GUI VE
+	//MA KIEU DU LIEU CUA BIEN NHAN LA STRING:
+		//NULL: KO KEY-KO VALUE
+		//EMPTY: CO KEY-KO VALUE
+	//... CAC KIEU DU LIEU CON LAI THI AUTO NULL NEU KO BAT KI FIELD HOAC VALUE = EMPTY 
+	// AUTO-NULL
+	@PostMapping(value="api/building")
+	public Object getBuilding(@RequestBody Buildingdto request) {
 		//Xu li duoi db rui`
 		//h tra ve cho client
-		Buildingdto response = new Buildingdto();
-		response.setName(name);
-		response.setNumberOfBasement(noB);
-		response.setWard(ward);
-		response.setStreet(street);
-		return response;
+		try {
+			validate(request);
+		} catch (FieldRequiredExeption e) {
+			ErrorResponseDto err = new ErrorResponseDto();
+			err.setError(e.getMessage());
+			List<String> details = new ArrayList<String>();
+			details.add("Check again name or numberOfBasement because u have null field");
+			
+			err.setDetails(details);
+			return err;
+		}
+		return null;
 	}
 	
-	//khong can ghi tung param de filter
-	@PostMapping(value="api/building")
-	public Buildingdto getBuildingV2(@RequestBody Buildingdto request) {
-		return request;
+	private void validate(Buildingdto request) throws FieldRequiredExeption {
+		if(request.getName().isEmpty() || request.getName() == null || request.getNumberOfBasement() == null)
+			throw new FieldRequiredExeption("name or number of base is null");
 	}
+	
+	
+	//khong can ghi tung param de filter
+//	@PostMapping(value="api/building")
+//	public Buildingdto getBuildingV2(@RequestBody Buildingdto request) {
+//		return request;
+//	}
 	
 	@DeleteMapping("api/building/{id}/{name}")
 	public void deleteBuilding(@PathVariable Integer id,
---
+++ src/main/java/com/dontwait/spring_boot_non_jwt/exceptions/FieldRequiredExeption.java
@@ -0,0 +1,7 @@
+package com.dontwait.spring_boot_non_jwt.exceptions;
+
+public class FieldRequiredExeption extends Exception{
+	public FieldRequiredExeption(String s) {
+		super(s);
+	}
+}
